793bb1e8c0daf82d6aec80845594f537dfcf0338
QubeQode <mohammad.razi.fakih@gmail.com>
2026-06-29 02:59:37 -0700

    create TaskManagerApplication frontend

---
+++ TaskManagerApplication/task-manager-frontend/exercises.ts
@@ -0,0 +1,29 @@
+enum Priority{
+    Low,
+    Medium,
+    High
+}
+
+interface Task {
+    Id: number;
+    Name: string;
+    Description: string;
+    Completed: boolean;
+    Priority: Priority,
+    DueDate: Date,
+    CreatedAt: Date
+}
+
+const generateId = (previousId: number): number => {
+    if (previousId = 0)
+        return 0;
+    else
+    return previousId + 1;
+};
+
+const calculateDateDiff = (completeDate: Date, createDate: Date): number =>
+     completeDate.getTime() - createDate.getTime();
+
+function firstItem<T> (array: T[]): T | undefined {
+    return array[0];
+}
